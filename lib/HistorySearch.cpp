11df7dc5cbb358b6f98537a699678484447447e0
kalaksi <kalaksi@users.noreply.github.com>
2025-09-02 19:59:55 +0300

    Fix search broken in Qt6 migration

--- lib/HistorySearch.cpp
+++ lib/HistorySearch.cpp
@@ -41,7 +41,7 @@ HistorySearch::~HistorySearch() {
 void HistorySearch::search() {
     bool found = false;
 
-    if( ! m_regExp.isValid())
+    if( ! m_regExp.pattern().isEmpty())
     {
         if (m_forwards) {
             found = search(m_startColumn, m_startLine, -1, m_emulation->lineCount()) || search(0, 0, m_startColumn, m_startLine);
